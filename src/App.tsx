23b3a6b3baa1c77f3325adc21408c56a842333d2
Kinga Kuś <kingakus@jabuszko-kk.home>
2022-12-29 22:08:27 +0100

    feat: :sparkles: Added ActualEvents componet

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import AboutUs from './components/AboutUs';
+import ActualEvents from './components/ActualEvents';
 
 function App() {
   return (
     <>
       <Banner />
       <AboutUs />
+      <ActualEvents />
     </>
   );
 }
--- src/components/ActualEvents.tsx
+++ src/components/ActualEvents.tsx
@@ -1,27 +1,112 @@
-import { Box, Grid, Typography } from '@mui/material';
-import ActualEvent1 from '../../assets/event1.png';
-import ActualEvent2 from '../../assets/event2.png';
-import ActualEvent3 from '../../assets/event3.png';
+import { Box, Typography, Container, styled } from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
+import actualEvent1 from '../assets/event1.png';
+import actualEvent2 from '../assets/event2.png';
+import actualEvent3 from '../assets/event3.png';
+
+const EventsBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-around',
+  width: '70%',
+  marginTop: theme.spacing(5),
+  [theme.breakpoints.down('md')]: {
+    width: '100%',
+    flexDirection: 'column'
+  }
+}));
+
+const EventBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: theme.spacing(5),
+  [theme.breakpoints.down('sm')]: {
+    margin: theme.spacing(2, 0, 2, 0)
+  }
+}));
 
 const ActualEvents = () => {
   return (
-    <Box>
-      <Box sx={{ display: "flex" }}>
-        <Typography variant="h3" color="red">AKTUALNE&nbsp; &nbsp;</Typography>
-        <Typography variant="h3">WYDARZENIA</Typography>
-      </Box>
-      <Grid container spacing={0}>
-        <Grid>
-          <img src={ActualEvent1} alt="Actual event number 1" />
-        </Grid>
-        <Grid>
-          <img src={ActualEvent2} alt="Actual event number 2" />
-        </Grid>
-        <Grid>
-          <img src={ActualEvent3} alt="Actual event number 3" />
-        </Grid>
-      </Grid>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        my: 10
+      }}
+    >
+      <Typography
+        variant="h3"
+        sx={{
+          fontSize: '35px',
+          fontWeight: 'bold',
+          color: '#000339',
+          my: 3
+        }}
+      >
+        Aktualne wydarzenia
+      </Typography>
+      <EventsBox>
+        <EventBox>
+          <img src={actualEvent1} alt="actualEvent1" />
+          <Box
+            sx={{
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', fontSize: '14px', color: '#0689FF' }}
+            >
+              Więcej
+            </Typography>
+            <ArrowRightAltIcon style={{ color: '#0689FF' }} />
+          </Box>
+        </EventBox>
+        <EventBox>
+          <img src={actualEvent2} alt="actualEvent2" />
+          <Box
+            sx={{
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', fontSize: '14px', color: '#0689FF' }}
+            >
+              Więcej
+            </Typography>
+            <ArrowRightAltIcon style={{ color: '#0689FF' }} />
+          </Box>
+        </EventBox>
+        <EventBox>
+          <img src={actualEvent3} alt="actualEvent1" />
+          <Box
+            sx={{
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'bold', fontSize: '14px', color: '#0689FF' }}
+            >
+              Więcej
+            </Typography>
+            <ArrowRightAltIcon style={{ color: '#0689FF' }} />
+          </Box>
+        </EventBox>
+      </EventsBox>
     </Box>
   );
 };
