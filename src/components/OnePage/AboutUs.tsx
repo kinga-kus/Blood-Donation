f5dc21d9b0e4a23b36394e8614e21569ce3f21d9
kinga-kus <103138106+kinga-kus@users.noreply.github.com>
2023-01-19 17:50:57 +0100

    [KK-0025]: 💄 feat: Changes the look of AboutUs component and added TtilesAndHeader… (#30)
    * feat: Changes the look of AboutUs component and added TtilesAndHeaders component
    
    * feat: change the array and map method of images
    
    Co-authored-by: Kinga Kuś <kingakus@jabuszko-kk.home>

--- src/components/OnePage/AboutUs.tsx
+++ src/components/OnePage/AboutUs.tsx
@@ -1,70 +1,81 @@
 import React from 'react';
-import { Box, styled, Typography } from '@mui/material';
+import { Box, Grid, Paper, styled, Typography } from '@mui/material';
+import { CustomHeader, CustomQuote } from '../TitlesAndHeaders';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+
+const images = [image1, image2];
 
 const CustomBox = styled(Box)(({ theme }) => ({
-  width: '60%',
-  [theme.breakpoints.down('md')]: {
-    width: '85%'
+  padding: '60px 50px',
+  marginTop: '80px',
+  backgroundColor: '#EEE5E9',
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    alignItems: 'left',
+    textAlign: 'left',
+    padding: theme.spacing(5)
   }
 }));
 
+const CustomTextBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  marginTop: '80px',
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    alignItems: 'left',
+    textAlign: 'left',
+    padding: theme.spacing(5)
+  },
+  [theme.breakpoints.down('md')]: {
+    padding: theme.spacing(5),
+    flexDirection: 'column',
+    alignItems: 'left',
+    textAlign: 'left'
+  }
+}));
+
+const CustomGrid = styled(Grid)(({ theme }) => ({
+  marginTop: '84px',
+  flexGrow: 1,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}));
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%'
+});
+
 const AboutUs = () => {
   return (
-    <Box
-      id="mission"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        my: 10
-      }}
-    >
-      <Typography
-        variant="h3"
-        sx={{
-          fontSize: '35px',
-          fontWeight: 'bold',
-          color: '#000339',
-          my: 3
-        }}
-      >
-        Misja
-      </Typography>
-      <CustomBox>
-        <Typography
-          variant="body2"
-          sx={{
-            fontSize: '16px',
-            fontWeight: 400,
-            color: '#5A6473',
-            textAlign: 'center'
-          }}
-        >
-          Zadaniem priorytetowym Centrum Krwiodawstwa w Krakowie jest pobieranie
-          krwi i jej składników od dawców krwi oraz przetwarzanie jej w celu
-          wytworzenia preparatów niezbędnych dla ratowania życia i zdrowia
-          pacjentów wymagających transfuzji.
-        </Typography>
-      </CustomBox>
-      <CustomBox>
-        <Typography
-          variant="body2"
-          sx={{
-            fontSize: '16px',
-            fontWeight: 400,
-            color: '#5A6473',
-            textAlign: 'center',
-            my: 5
-          }}
-        >
-          Procedury stosowane do wytworzenia składników krwi na które składa
-          się: kwalifikacja krwiodawcy, pobranie krwi, jej przetworzenie,
-          zbadanie, przechowanie i transportowanie gwarantują najwyższą jakość i
-          bezpieczeństwo dla biorców.
-        </Typography>
-      </CustomBox>
-    </Box>
+    <CustomBox>
+      <Box>
+        <CustomHeader headerText="About Us" />
+        <CustomTextBox>
+          <CustomQuote quoteText="Your help will be a gift for the others" />
+          <Typography sx={{ fontSize: 16, fontFamily: 'Poppins' }}>
+            The priority task of the Blood Donation Centre in Krakow is to
+            collect blood and its components from blood donors and process it to
+            produce preparations necessary to save the life and health of
+            patients requiring transfusions. The procedures used for the
+            production of blood components, which consist of: qualification of
+            the donor, collection, processing, testing, storage and transport of
+            blood, guarantee the highest quality and safety for the recipients.
+          </Typography>
+        </CustomTextBox>
+      </Box>
+      <CustomGrid container spacing={2} columns={{ xs: 1, sm: 1, md: 8 }}>
+        {images.map((img) => (
+          <Grid item xs={1} md={4}>
+            <Img src={img} />
+          </Grid>
+        ))}
+      </CustomGrid>
+    </CustomBox>
   );
 };
 
