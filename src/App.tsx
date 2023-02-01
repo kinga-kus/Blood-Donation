e95d5586f2495ea5171196806c35aa1a58d7e7cb
Kinga Kuś <kingakus@jabuszko-kk.home>
2023-02-01 10:26:56 +0100

    feat: Modification of Departments component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+
+import Departments from './components/FotterLinks/Departments';
+import Gallery from './components/FotterLinks/Gallery';
+import Questions from './components/FotterLinks/Questions';
 import MoreInformation from './components/MoreInformation/MoreInformation';
 import OnePage from './components/OnePage';
+import RegisterSteps from './components/Registeration/RegisterSteps';
 import Operation from './components/Registeration/RegisterSteps';
 
 function App() {
@@ -13,6 +18,10 @@ function App() {
       <Route path="/Blood-Donation" element={<OnePage />} />
       <Route path="/rejestracja" element={<Operation />} />
       <Route path="/informacje" element={<MoreInformation />} />
+      <Route path="/Blood-Donation/departments" element={<Departments />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/questions" element={<Questions />} />
+      <Route path="/registeration" element={<RegisterSteps />} />
     </Routes>
   );
 }
