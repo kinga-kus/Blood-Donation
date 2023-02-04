1c0e719df665eaceda680af8affe2b30703a6747
Kinga Kuś <kingakus@jabuszko-kk.home>
2023-02-04 16:28:47 +0100

    chore: Removed lines

--- src/components/FotterLinks/Gallery.tsx
+++ src/components/FotterLinks/Gallery.tsx
@@ -141,8 +141,6 @@ const Img = styled('img')({
 });
 
 const Item = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary
